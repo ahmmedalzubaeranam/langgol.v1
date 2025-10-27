4b9cbbad1554f0a9f115ecb3671231f4f96ba7c4
ahmmedalzubaeranam <ahmmedalzubaeranam@gmail.com>
2025-10-27 11:30:34 +0600

    Fix

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+import { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
 
-app.get('/test', (req, res) => {
+app.get('/test', (req: Request, res: Response) => {
   res.json({ message: 'Hello from the backend!' });
 });
 
